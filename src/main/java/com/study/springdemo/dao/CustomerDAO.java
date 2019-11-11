8c1e1c10b147c6826f67097de958d4a0291ade5c
netelli <natalli.dev@gmail.com>
2019-11-11 15:33:39 +0200

    Initial commit

---
+++ src/main/java/com/study/springdemo/dao/CustomerDAO.java
@@ -0,0 +1,17 @@
+package com.study.springdemo.dao;
+
+import java.util.List;
+
+import com.study.springdemo.entity.Customer;
+
+public interface CustomerDAO {
+
+	public List<Customer> getCustomers();
+
+	public void saveCustomer(Customer theCustomer);
+
+	public Customer getCustomer(int theId);
+
+	public void deleteCustomer(int theId);
+	
+}
